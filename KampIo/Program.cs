12fe41fc116bec98ce48659ca8e292ffc0ed1320
Can Balcı <canbalci@outlook.com.tr>
2021-01-19 15:48:40 +0300

    Add project files.

---
+++ KampIo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace KampIo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustumerManger customerManager = new CustumerManger();
+            customerManager.Logger = new DataBaseLogger();
+            customerManager.Add();
+            Console.ReadLine();
+
+            
+        }
+    }
+
+    class CustumerManger
+    {
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Custumer Add");
+
+        }
+
+
+    }
+
+    class DataBaseLogger : ILogger
+    {
+        public void Log()
+        {
+
+            Console.WriteLine("Logged to database");
+        }
+    }
+    class FileLoger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file");
+        }
+    }
+    class SmsLoger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to SMS");
+        }
+    }
+    class Logger : ILogger
+    {
+        public void Log()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    interface ILogger
+    {
+
+        void Log();
+
+    }
+
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi " +" : " + urun.Adi);
+        }
+
+        public void Ekle2(string urunadi, string aciklama, double fiyat,int StokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urunadi);
+        }
+
+    }
+}
